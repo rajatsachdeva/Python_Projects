033984ee6784e60d45a6831cc85619bfb84b656a
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-07-27 16:43:45 +0530

    added utils.py

--- src/fetch_data_from_twitter.py
+++ src/fetch_data_from_twitter.py
@@ -1,48 +1,22 @@
 #!/bin/user/python
 
 #import required libraries
+import utils as ut
 import signal
-import sys 
+import sys
 import time
 from tweepy.streaming import StreamListener
-from tweepy import OAuthHandler
 from tweepy import Stream
 
-# File path where fetched data will be stored
-FILEPATH="../data/twitter_data.txt"
-
-# Secret File Path
-SECRETFILEPATH="../secret/auth.txt"
-
 # Open the file
-f=open(FILEPATH, 'w')
-
-# Fetch the access token and consumer key from secret folder
-def fetch_secret():
-
-	f = open(SECRETFILEPATH, 'r')
-	authdict = {}
-
-	for data in f.readlines():
-		list = data.split("=")
-		authdict[list[0]] = list[1].strip()
-	
-	access_token        = authdict['access_token']
-	access_token_secret = authdict['access_token_secret']
-	consumer_key        = authdict['consumer_key']
-	consumer_secret     = authdict['consumer_secret']
-
-	auth = OAuthHandler(consumer_key, consumer_secret)
-	auth.set_access_token(access_token, access_token_secret)
-
-	return auth
+f=open(ut.FILEPATH, 'w')
 
 # Basic listener that just prints received tweets to stdout
 class TwitterListener (StreamListener):
 
 	def __init__(self, phrases):
 		# Get access to twitter api
-		auth = fetch_secret()
+		auth = ut.fetch_secret()
 		self.__stream = Stream(auth, listener=self)
 		print("Fetching Tweets...")
 		self.__stream.filter(track=phrases)
@@ -57,18 +31,6 @@ class TwitterListener (StreamListener):
 		print ("ERROR: ",status)
 		f.write (str(status))
 
-
-# Number of tweets captured
-def GetNumOfTweets():
-	f = open(FILEPATH, "r")
-	count = 0
-	for i in f.readlines():
-		count = count + 1
-
-	print(str(count)+" Tweets fetched !")
-	f.close()
-
-
 # Signal Handler
 def exit_gracefully(signum, frame):
     # restore the original signal handler as otherwise evil things will happen
@@ -78,8 +40,8 @@ def exit_gracefully(signum, frame):
 	try:
 		if input("\nReally quit? (y/n)> ").lower().startswith('y'):
 			print("Shutting down...")
-			GetNumOfTweets()
 			f.close()
+			ut.GetNumOfTweets()
 			#stream.disconnect()
 			sys.exit(1)
 
